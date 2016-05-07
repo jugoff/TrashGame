6e09eefbc728ed036308596d0bf24026f26c339d
VarUnicorn <benji.thiebaut@gmail.com>
2016-05-07 23:05:14 +0200

    added sprites management

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var game = new Phaser.Game(1536, 2048, Phaser.CANVAS, 'gameDiv', { preload: preload, create: create, update: update }); // Objet jeu de type Phaser & dimensions iPad
+var game = new Phaser.Game($(document).width(), $(document).height(), Phaser.CANVAS, 'gameDiv', { preload: preload, create: create, update: update }); // Objet jeu de type Phaser & dimensions iPad
 
 game.transparent = true;
 
 var score = 0;
-var scoreText, ordures, recycles;
+var scoreText, ordures, recycles, spritePos, conteneur;
 var gameSpeed = 2; // time before item spawn
 
 
@@ -17,27 +17,33 @@ tools.rand = function(min, max) {
 function preload() {
     
     scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    scoreText.anchor.setTo(0.5, 0.5);
+    scoreText.visible = true;
     
     /*************************** GAME SCALE ****************************/
-    /*
-    game.stage.scaleMode = Phaser.StageScaleMode.SHOW_ALL;
-    game.stage.scale.setShowAll();
-    window.addEventListener('resize', function () {
-        game.stage.scale.refresh();
-    });
-    game.stage.scale.refresh();
-    */
+   	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    //game.scale.setScreenSize();
     /***************************** SPRITES *****************************/
-    game.load.image('background', 'param/img/background/background.jpg');
-    
-	game.load.image('garbage-1', 'param/img/ordure/bouteille.png');
-	game.load.image('garbage-2', 'param/img/ordure/burger.png');    
-	game.load.image('garbage-3', 'param/img/ordure/fer_repasser.png');    
 
-	game.load.image('recycle-1', 'param/img/recycle/boiteOeufs.png');    
+	game.load.image('background', 'param/img/background/background.jpg');
+    
+    game.load.image('conteneur-jaune', 'param/img/conteneur-jaune.png');
+    
+	game.load.image('garbage-1', 'param/img/ordure/ampoule.png');
+	game.load.image('garbage-2', 'param/img/ordure/banane.png');    
+	game.load.image('garbage-3', 'param/img/ordure/batterie.png');    
+	game.load.image('garbage-4', 'param/img/ordure/bouteille.png');    
+	game.load.image('garbage-5', 'param/img/ordure/burger.png');    
+	game.load.image('garbage-6', 'param/img/ordure/fer_repasser.png');    
+	game.load.image('garbage-7', 'param/img/ordure/pile.png');    
+	game.load.image('garbage-8', 'param/img/ordure/porcelaine.png');    
+	game.load.image('garbage-9', 'param/img/ordure/pot_fleur.png');    
+	game.load.image('garbage-10', 'param/img/ordure/sac.png');    
+	game.load.image('garbage-11', 'param/img/ordure/tv.png');    
+
+ 	game.load.image('recycle-1', 'param/img/recycle/boiteOeufs.png');    
     game.load.image('recycle-2', 'param/img/recycle/canette.png');    
     game.load.image('recycle-3', 'param/img/recycle/carton.png');
-
     
     
 }
@@ -47,6 +53,9 @@ function create() {
    	game.physics.arcade.gravity.y = 200;
 
     game.add.sprite(0, 0, 'background');
+    conteneur = game.add.sprite(0 , 0, 'conteneur-jaune');
+    conteneur.x = (game.width / 2) - (conteneur.width / 2);
+    conteneur.y = game.height - conteneur.height;
     
 
     ordures = game.add.group();
@@ -61,7 +70,8 @@ function create() {
 }
 
 function update() {
-    
+    updatePositionsGroups(ordures);
+    updatePositionsGroups(recycles);
 }
 
 function render() {
@@ -69,7 +79,7 @@ function render() {
 }
 
 function eliminate (element) {
-    //element.kill();
+    element.kill();
 }
 
 function randomSpawn(){
@@ -87,8 +97,8 @@ function randomSpawnGarbage(nb){
 	for (var i = 0; i < nb; i++){
 		item = ordures.create(tools.rand(0, game.width), 0, 'garbage-' + tools.rand(1, 3));	
 		game.physics.arcade.enable(item);
-	}
-	ordures.callAll('events.onInputDown.add', 'events.onInputDown', eliminate);
+		item.events.onOutOfBounds.add( eliminate, this );
+	}	
 }
 
 function randomSpawnRecycle(nb){
@@ -98,7 +108,39 @@ function randomSpawnRecycle(nb){
 	for (var i = 0; i < nb; i++){
 		item = recycles.create(tools.rand(0, game.width), 0, 'garbage-' + tools.rand(1, 3));	
 		game.physics.arcade.enable(item);
-		
+		item.events.onOutOfBounds.add( eliminate, this );	
 	}
-	ordures.callAll('events.onInputDown.add', 'events.onInputDown', eliminate);
 }
+
+function updatePositionsGroups(group){
+	group.forEach(function(elem) {
+		if (elem.alive){
+			spritePos = ( elem.x + (elem.width / 2));
+			if (spritePos >= 0 && spritePos <= (game.width / 12)*1) {
+            	elem.x += 6;
+	        } else if (spritePos >= (game.width / 12)*1 && spritePos <= (game.width / 12)*2) {
+	            elem.x += 5;
+	        } else if (spritePos >= (game.width / 12)*2 && spritePos <= (game.width / 12)*3) {
+	            elem.x += 4;
+	        } else if (spritePos >= (game.width / 12)*3 && spritePos <= (game.width / 12)*4) {
+	            elem.x += 3;
+	        } else if (spritePos >= (game.width / 12)*4 && spritePos <= (game.width / 12)*5) {
+	            elem.x += 2;
+	        } else if (spritePos >= (game.width / 12)*5 && spritePos <= (game.width / 12)*6) {
+	            elem.x += 1;
+	        } else if (spritePos >= (game.width / 12)*6 && spritePos <= (game.width / 12)*7) {
+	            elem.x -= 1;
+	        } else if (spritePos >= (game.width / 12)*7 && spritePos <= (game.width / 12)*8) {
+	            elem.x -= 2;
+	        } else if (spritePos >= (game.width / 12)*8 && spritePos <= (game.width / 12)*9) {
+	            elem.x -= 3;
+	        } else if (spritePos >= (game.width / 12)*9 && spritePos <= (game.width / 12)*10) {
+	            elem.x -= 4;
+	        } else if (spritePos >= (game.width / 12)*10 && spritePos <= (game.width / 12)*11) {
+	            elem.x -= 5;
+	        } else if (spritePos >= (game.width / 12)*11 && spritePos <= (game.width / 12)*12) {
+	            elem.x -= 6;
+	        }
+		}
+	});
+} 
